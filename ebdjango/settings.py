9f46e1317e5a7442c6ad7015617b248f460079b0
qf2050 <qf2050@nyu.edu>
2024-02-22 12:03:30 +0800

    thrid commit

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['django-env.eba-b6yhyiy6.us-west-2.elasticbeanstalk.com','127.0
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
